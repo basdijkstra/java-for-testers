ddcf66f7c6f5518b138b4aa10a4946da89a3b4b5
Bas Dijkstra <bas@ontestautomation.com>
2021-03-29 08:49:13 +0200

    Updated inheritance exercise

--- src/main/java/exercises/Guitar.java
+++ src/main/java/exercises/Guitar.java
@@ -1,6 +1,6 @@
 package exercises;
 
-public class Guitar extends Instrument {
+public class Guitar {
 
     // TODO: make the Guitar class inherit from the Instrument class
 
@@ -18,6 +18,6 @@ public class Guitar extends Instrument {
 
         // TODO: implement this method so it returns "This black guitar has 6 strings and costs 1000 dollars"
         //   use the getters from both the Guitar and the Instrument class to achieve this
-        return String.format("This %s guitar has %d strings and costs %d dollars", this.getColor(), this.getNumberOfStrings(), this.getPrice());
+        return "";
     }
 }
--- src/test/java/exercises/GuitarTest.java
+++ src/test/java/exercises/GuitarTest.java
@@ -8,13 +8,10 @@ public class GuitarTest {
     public void testInstrumentInfo_ShouldReturnCorrectDescription() {
 
         // ARRANGE - Create a new Guitar object
-        Guitar guitar = new Guitar();
 
         // ACT - Call the getInstrumentInfo() method and store the result in a String variable
-        String info = guitar.getInstrumentInfo();
 
         // ASSERT - Check that the info equals 'This black guitar has 6 strings and costs 1000 dollars'
-        Assert.assertEquals("This black guitar has 6 strings and costs 1000 dollars", info);
     }
 
     @Test
